eb60719929d9976165bc3ed9f7d3a7fcce7d21a6
huydoan <dthanhhuy132@gmail.com>
2023-01-02 23:07:15 +0700

    using cors

--- app.js
+++ app.js
@@ -26,16 +26,8 @@ const corsOpts = {
   allowedHeaders: ["Content-Type"],
 };
 
-app.use(cors(corsOpts));
+app.use(cors);
 morganBody(app);
-
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
 
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
