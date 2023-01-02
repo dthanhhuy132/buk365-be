f37836f8d7282a5bf7722040ca2d7ed941477193
huydoan <dthanhhuy132@gmail.com>
2023-01-02 12:31:35 +0700

    change port

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import connectDB from './config/DB.js';
-import morganBody from 'morgan-body';
-import root from './routes/root.js';
-import path from 'path';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import connectDB from "./config/DB.js";
+import morganBody from "morgan-body";
+import root from "./routes/root.js";
+import path from "path";
 
 // set up dependencies
 const app = express();
@@ -22,9 +22,11 @@ morganBody(app);
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use('/api', root);
-app.use('/image', express.static(path.join(__dirname, 'uploads')));
+app.use("/api", root);
+app.use("/image", express.static(path.join(__dirname, "uploads")));
 
-app.listen(PORT, () => console.log(`Server connect port ${PORT}`));
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Server connect port ${PORT}`)
+);
 
 export default app;
