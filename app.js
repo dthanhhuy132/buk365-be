ad285dbfc4c47d9576465b97a0a11e359d75cd59
huydoan <dthanhhuy132@gmail.com>
2023-01-02 13:26:45 +0700

    change cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 connectDB();
 
 //middleware
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 morganBody(app);
 
 const PORT = process.env.PORT || 5000;
@@ -25,8 +25,6 @@ const __dirname = path.resolve();
 app.use("/api", root);
 app.use("/image", express.static(path.join(__dirname, "uploads")));
 
-app.listen(process.env.PORT || 5000, () =>
-  console.log(`Server connect port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server connect port ${PORT}`));
 
 export default app;
